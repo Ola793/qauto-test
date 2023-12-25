e0aa407a595a5cc78183b834e2f3a61069812615
Olha Teplova <olitieplove@gmail.com>
2023-12-25 16:16:09 +0200

    fix: add deleted files

---
+++ cypress/e2e/change-photo.spec.js
@@ -0,0 +1,20 @@
+/// <reference types = "cypress" />
+
+describe("Change photo test", () => {
+  const authData = `${Cypress.env("authName")}:${Cypress.env("authPass")}`;
+
+  it.only("photo change", () => {
+    cy.log("photo change");
+    cy.visit(`https://${authData}@qauto.forstudy.space/`);
+    cy.request("POST", "https://qauto.forstudy.space/api/auth/signin", {
+      email: Cypress.env("loginEmail"),
+      password: Cypress.env("loginPass"),
+      remember: false,
+    });
+
+    cy.visit(`https://${authData}@qauto.forstudy.space/panel/profile`);
+    cy.contains("button", "Edit profile").click();
+    cy.get("input#editProfilePhoto").selectFile("cypress/fixtures/ava.png");
+    cy.contains("Save").click();
+  });
+});
