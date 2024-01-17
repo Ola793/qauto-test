69029da614e8ffc8b373cf7120e6169f7f0db06a
Olha Teplova <olitieplove@gmail.com>
2024-01-18 00:18:32 +0200

    fix: spec

--- cypress/e2e/change-photo.spec.js
+++ cypress/e2e/change-photo.spec.js
@@ -1,33 +1,14 @@
 /// <reference types = "cypress" />
 
-// describe("Change photo test", () => {
-//   const authData = `${Cypress.env("authName")}:${Cypress.env("authPass")}`;
-
-//   it("photo change", () => {
-//     cy.log("photo change");
-//     cy.visit(`https://${authData}@qauto.forstudy.space/`);
-//     cy.request("POST", "https://qauto.forstudy.space/api/auth/signin", {
-//       email: Cypress.env("loginEmail"),
-//       password: Cypress.env("loginPass"),
-//       remember: false,
-//     });
-
-//     cy.visit(`https://${authData}@qauto.forstudy.space/panel/profile`);
-//     cy.contains("button", "Edit profile").click();
-//     cy.get("input#editProfilePhoto").selectFile("cypress/fixtures/ava.png");
-//     cy.contains("Save").click();
-//   });
-// });
-
 describe("Change photo test", () => {
-  const authData = `${CYPRESS_authName}:${CYPRESS_authPass}`;
+  const authData = `${Cypress.env("authName")}:${Cypress.env("authPass")}`;
 
   it("photo change", () => {
     cy.log("photo change");
     cy.visit(`https://${authData}@qauto.forstudy.space/`);
     cy.request("POST", "https://qauto.forstudy.space/api/auth/signin", {
-      email: CYPRESS_loginEmail,
-      password: CYPRESS_loginPass,
+      email: Cypress.env("loginEmail"),
+      password: Cypress.env("loginPass"),
       remember: false,
     });
 
